b33c4fb765fa2ed851c7dd170a15701d768881c8
Zin Zin Than <zinzinn1507@gmail.com>
2023-11-24 17:11:01 +0630

    RestAPI [AdoDotNet] Get

---
+++ ZZTDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -0,0 +1,103 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using ZZTDotNetCore.RestApi.Models;
+
+namespace ZZTDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogAdoDotNetController : ControllerBase
+    {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder;
+        
+        public BlogAdoDotNetController()
+        {
+            sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = "DESKTOP-2RCKCTJ\\SQLSERVER",
+                InitialCatalog = "DotNetCore",
+                UserID = "sa",
+                Password = "sasa"
+            };
+        }
+        
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = "select * from tbl_blog";
+            SqlCommand cmd = new SqlCommand(query,connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            List<BlogDataModel> lst = new List<BlogDataModel>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                BlogDataModel item = new BlogDataModel();
+                item.Blog_Id = Convert.ToInt32( row["Blog_Id"]);
+                item.Blog_Title = row["Blog_Title"].ToString();
+                item.Blog_Author = row["Blog_Title"].ToString();
+                item.Blog_Content = row["Blog_Content"].ToString();
+                lst.Add(item);
+
+            }
+            BlogListResponseModel model = new BlogListResponseModel
+            {
+                IsSuccess = true,
+                Message = "Success",
+                Data =lst
+            };
+            return Ok(model);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            string query = "select * from tbl_blog where Blog_Id=@Blog_Id";
+            using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            BlogDataModel? item = db.Query<BlogDataModel>(query, new BlogDataModel { Blog_Id = id }).FirstOrDefault();
+
+            if (item is null)
+            {
+                var response = new { IsSuccess = false, Message = "No data found." };
+                return NotFound(response);
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogDataModel blog)
+        {
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([Blog_Title]
+           ,[Blog_Author]
+           ,[Blog_Content])
+    VALUES
+           (@Blog_Title
+            ,@Blog_Author
+            ,@Blog_Content)";
+
+            using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, blog);
+
+            BlogResponseModel model = new BlogResponseModel
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "Saving Successful." : "Saving Failed.",
+                Data = blog
+            };
+            return Ok(model);
+        }
+
+    }
+}
