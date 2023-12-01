1e02641ae44032052014fb36f7a2ec743ee1d0a1
Zin Zin Than <zinzinn1507@gmail.com>
2023-12-01 15:24:16 +0630

    [Complete] sweetalert with CRUD

--- ZZTDotNetCore.HtmlApp/js/custom.js
+++ ZZTDotNetCore.HtmlApp/js/custom.js
@@ -14,36 +14,22 @@ function warningMessage(message) {
     });
 }
 
+function confirmMessage(message) {
+    return new Promise((resolve, reject) => {
+        Swal.fire({
+            title: "Confirm",
+            text: message,
+            icon: "question",
+            showCancelButton: true,
+        }).then((result) => {
+            resolve(result.isConfirmed)
+        });
+    });
+
+}
 function uuidv4() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
 
-function readUser() {
-    $("#tbTbody").html('');
-
-    var users = localStorage.getItem("tbl_user");
-    users = JSON.parse(users);
-
-    let htmlString = '';
-    $.each(users, function(index, value) {
-        console.log(index + ": " + value.UserName);
-
-        htmlString += `
-            <tr>
-                <td>
-                    <buttom type="buttom" class="btn btn-warning" onclick="editUser('${value.UserId}')">
-                        <i class="fa-solid fa-pen-to-square"></i>
-                    </buttom>
-                    <buttom type="buttom" class="btn btn-danger">
-                        <i class="fa-solid fa-trash"></i>
-                    </buttom>
-                </td>
-                <td>${index + 1}</td>
-                <td>${value.UserName}</td>
-            </tr>`
-    });
-
-    $("#tbTbody").html(htmlString);
-}
