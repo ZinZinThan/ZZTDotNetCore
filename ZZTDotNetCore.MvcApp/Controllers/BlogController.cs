54713e19dbb80d85e4da0903498918f2f40dc505
Zakawar <thihathurazakawarmaung@gmail.com>
2023-12-07 22:35:56 +0630

    MVC-Retrieve

---
+++ ZZTDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZZTDotNetCore.MvcApp.EFDbContext;
+using ZZTDotNetCore.MvcApp.Models;
+
+namespace ZZTDotNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            List<BlogDataModel> lst = _context.Blogs.ToList();
+            return View("BlogIndex", lst);
+        }
+    }
+}
