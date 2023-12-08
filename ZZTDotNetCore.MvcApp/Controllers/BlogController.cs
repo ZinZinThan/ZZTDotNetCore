8d0f88db75289d78c9b0c4aa00af169057c66bac
Zin Zin Than <zinzinn1507@gmail.com>
2023-12-08 15:13:46 +0630

    MVC =>create

--- ZZTDotNetCore.MvcApp/Controllers/BlogController.cs
+++ ZZTDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -19,5 +19,24 @@ namespace ZZTDotNetCore.MvcApp.Controllers
             List<BlogDataModel> lst = _context.Blogs.ToList();
             return View("BlogIndex", lst);
         }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> BlogSave(BlogDataModel reqModel)
+        {
+            await _context.Blogs.AddAsync(reqModel);
+            var result = await _context.SaveChangesAsync();
+            string message = result > 0 ? "Saving Successful." : " Saving Failed.";
+            TempData["Message"] = message;
+            TempData["IsSuccess"] = result > 0;
+
+            return Redirect("/blog");
+        }
     }
 }
