755f26dacc1ccf55e8af14dee715b80855c64f11
Zakawar <thihathurazakawarmaung@gmail.com>
2023-12-16 23:53:15 +0630

    [Complete] Ajax Delete

--- ZZTDotNetCore.MvcApp/Controllers/BlogAjaxController.cs
+++ ZZTDotNetCore.MvcApp/Controllers/BlogAjaxController.cs
@@ -58,5 +58,94 @@ namespace ZZTDotNetCore.MvcApp.Controllers
             return Json(model); 
         }
 
+		[ActionName("Edit")]
+		public async Task<IActionResult> BlogEdit(int id)
+		{
+			if (!await _context.Blogs.AsNoTracking().AnyAsync(x => x.Blog_Id == id))
+			{
+				TempData["Message"] = "No data found.";
+				TempData["IsSuccess"] = false;
+				return Redirect("/blogajax/list");
+			}
+
+			var blog = await _context.Blogs.AsNoTracking().FirstOrDefaultAsync(x => x.Blog_Id == id);
+			if (blog is null)
+			{
+				TempData["Message"] = "No data found.";
+				TempData["IsSuccess"] = false;
+				return Redirect("/blogajax/list");
+			}
+
+			MessageModel model = new MessageModel(true,"data");
+
+			return Json(model,blog);
+
+			//return View("BlogEdit", blog);
+		}
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> BlogUpdate(int id, BlogDataModel reqModel)
+        {
+            if (!await _context.Blogs.AsNoTracking().AnyAsync(x => x.Blog_Id == id))
+            {
+                TempData["Message"] = "No data found.";
+                TempData["IsSuccess"] = false;
+                return Redirect("/blogajax/list");
+            }
+
+            var blog = await _context.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+            if (blog is null)
+            {
+                TempData["Message"] = "No data found.";
+                TempData["IsSuccess"] = false;
+                return Redirect("/blogajax/list");
+            }
+
+            blog.Blog_Title = reqModel.Blog_Title;
+            blog.Blog_Author = reqModel.Blog_Author;
+            blog.Blog_Content = reqModel.Blog_Content;
+
+            int result = _context.SaveChanges();
+
+            string message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            TempData["Message"] = message;
+            TempData["IsSuccess"] = result > 0;
+
+			MessageModel model = new MessageModel(result > 0, message);
+
+			return Json(model);
+		}
+
+        [ActionName("Delete")]
+        public async Task<IActionResult> BlogDelete(int id)
+        {
+            if (!await _context.Blogs.AsNoTracking().AnyAsync(x => x.Blog_Id == id))
+            {
+                TempData["Message"] = "No data found.";
+                TempData["IsSuccess"] = false;
+                return Redirect("/blogajax/list");
+            }
+
+            var blog = await _context.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+            if (blog is null)
+            {
+                TempData["Message"] = "No data found.";
+                TempData["IsSuccess"] = false;
+                return Redirect("/blogajax/list ");
+            }
+
+            _context.Remove(blog);
+            int result = _context.SaveChanges();
+
+            string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+            TempData["Message"] = message;
+            TempData["IsSuccess"] = result > 0;
+
+            MessageModel model = new MessageModel(result > 0, message);
+
+            return Json(model);
+        }
+
     }
 }
