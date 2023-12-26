ee107e245b7c8267617e9f461afe9b4070f279df
Zin Zin Than <zinzinn1507@gmail.com>
2023-12-26 16:21:19 +0630

    [Update] ATM UI

---
+++ ZZTDotNetCore.AtmApp/Controllers/AtmController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZZTDotNetCore.AtmApp.Controllers
+{
+    public class AtmController : Controller
+    {
+        [ActionName("Index")]
+        public IActionResult AtmIndex()
+        {
+            return View("AtmIndex");
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public IActionResult AtmLogin()
+        {
+            return Redirect("/atm/list");
+        }
+
+        [ActionName("Create")]
+        public IActionResult AtmCreate()
+        {
+            return View("AtmCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public IActionResult AtmSave()
+        {
+            return Redirect("/atm/index");
+        }
+
+        [ActionName("List")]
+        public IActionResult AtmList()
+        {
+            return View("AtmList");
+        }
+    }
+}
